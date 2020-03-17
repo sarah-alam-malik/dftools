72e014b7d239ed1fe2ad322f7170486ab226b671
Shane Breeze <s.breeze15@imperial.ac.uk>
2020-03-17 14:51:45 +0000

    Bump z version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as fh:
 
 setuptools.setup(
     name="dftools",
-    version="0.2.1",
+    version="0.2.2",
     author="Shane Breeze",
     author_email="sdb15@ic.ac.uk",
     scripts=[],
